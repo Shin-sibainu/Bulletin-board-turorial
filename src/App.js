5d9183f043f4086949fb23377f517d9214e31855
Shin-sibainu <shincode0712@gmail.com>
2021-08-30 13:10:05 +0900

    firstcommit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <h1>Mario Plan</h1>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
